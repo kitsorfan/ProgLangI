9493dfc002cc81a0267f11e38184ed7c0c7da5f0
kitsorfan <kitsorfan@protonmail.com>
2021-03-25 18:38:13 +0200

    add reading the maze_doors

--- 1st Seiries/loop_rooms/loop_rooms.cpp
+++ 1st Seiries/loop_rooms/loop_rooms.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 
-/* -----------------------MAIN IDEA---------------------------------------------
+/* ----------------------- MAIN IDEA -------------------------------------------
 
 We create a N x M array, called maze_doors. Each cell is marked with UDRL (up, down, right, left).
 We read the given file (from arguments) and fill that maze..
@@ -44,6 +44,31 @@ Note: Possibly it is true that '0'==='2'; we will check out later.
 
 int main(int argc, char** argv) { //arg = number of arguments, argv pointer array of pointers to arguments
 
+// ---------------------------------  DECLARATIONS -----------------------------
+  int N,M;//number of rooms
+  char maze_doors[1000][1000];
+//  int maze_escapes[1000][1000] ={ { 0 } }; // initializing to 0
 
 
+  const char * file_name = argv[1];	//name of file to open
+  ifstream File(file_name); 		//open the file
+  File>>N>>M;
+  for (int i=0; i<N; i++){  //reading all rooms
+    for (int j=0; j<M; j++){
+      File>>maze_doors[i][j];
+    }
+  }
+  //---------- optional
+  cout << "Our initial array:\n";
+  for (int i=0; i<N; i++){  //reading all rooms
+    for (int j=0; j<M; j++){
+      cout<<maze_doors[i][j]<<" ";
+    }
+    cout<<"\n";
+  }
+  cout<<"\n";
+  //--- optional end
+
+
+return 0;
 }
