a06ed41194aa4d0eb6f4650b1be8ffb42b796907
kitsorfan <kitsorfan@protonmail.com>
2021-03-25 20:40:55 +0200

    add function to find escapable rooms

--- 1st Seiries/loop_rooms/loop_rooms.cpp
+++ 1st Seiries/loop_rooms/loop_rooms.cpp
@@ -42,12 +42,53 @@ Note: Possibly it is true that '0'==='2'; we will check out later.
 */
 
 
+int check_neighbour(int array[][1000], int i, int j, int N, int M, char door){
+  int return_value=0;
+
+  if (array[i][j]==0){
+  switch(door) {
+      case 'L': // Left Door
+        if (j==0) return_value = 1;
+        else{
+            if (array[i][j-1]==1) return_value = 1;
+        }
+        break;
+
+
+      case 'R': // Right Door
+        if (j==(M-1)) return_value = 1;
+        else{
+            if (array[i][j+1]==1) return_value = 1;
+        }
+        break;
+
+      case 'U': // Up Door
+        if (i==0) return_value = 1;
+        else{
+            if (array[i-1][j]==1) return_value = 1;
+        }
+        break;
+
+      case 'D': // Down Door
+        if (i==(N-1)) return_value = 1;
+        else{
+            if (array[i+1][j]==1) return_value = 1;
+        }
+        break;
+    }
+    return return_value;
+  }
+  return 1;
+}
+
+
 int main(int argc, char** argv) { //arg = number of arguments, argv pointer array of pointers to arguments
 
 // ---------------------------------  DECLARATIONS -----------------------------
   int N,M;//number of rooms
   char maze_doors[1000][1000];
-//  int maze_escapes[1000][1000] ={ { 0 } }; // initializing to 0
+  int maze_escapes[1000][1000] = { { 0 } }; // initializing to 0
+  int escapable_rooms = 0; // number of rooms which can lead to the exit
 
 
   const char * file_name = argv[1];	//name of file to open
@@ -69,6 +110,45 @@ int main(int argc, char** argv) { //arg = number of arguments, argv pointer arra
   cout<<"\n";
   //--- optional end
 
+  //Scan from left to right (top->bottom)
+  for (int i=0; i<N; i++){
+    for (int j=0; j<M; j++){
+      maze_escapes[i][j]=check_neighbour(maze_escapes, i, j, N, M, maze_doors[i][j]);
+    }
+  }
 
-return 0;
+  //Scan from right to left (bottom->top)
+  for (int i=(N-1); i>=0; i--){
+    for (int j=(M-1); j>=0; j--){
+      maze_escapes[i][j]=check_neighbour(maze_escapes, i, j, N, M, maze_doors[i][j]);
+    }
+  }
+
+  //Scan from bottom to top (left->right)
+  for (int j=0; j<M; j++){
+    for (int i=(N-1); i>=0; i--){
+      maze_escapes[i][j]=check_neighbour(maze_escapes, i, j, N, M, maze_doors[i][j]);
+    }
+  }
+
+  //Scan from top to bottom (right->left)
+  for (int j=(M-1); j>=0; j--){
+    for (int i=0; i<N; i++){
+      maze_escapes[i][j]=check_neighbour(maze_escapes, i, j, N, M, maze_doors[i][j]);
+    }
+  }
+
+  //---------- optional
+  cout << "What we get:\n";
+  for (int i=0; i<N; i++){  //reading all rooms
+    for (int j=0; j<M; j++){
+      cout<<maze_escapes[i][j]<<" ";
+      escapable_rooms+=maze_escapes[i][j];
+    }
+    cout<<"\n";
+  }
+  cout<<"Escapable Rooms: "<<escapable_rooms<<"\n";
+  //--- optional end
+
+  return 0;
 }
