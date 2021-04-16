ade213fdfe3e0d22e9c5c2dc0df6d19727cec66e
stylkand <el17088@mail.ntua.gr>
2021-04-16 12:00:43 +0200

    akyro

--- 1st Seiries/longest/longest.cpp
+++ 1st Seiries/longest/longest.cpp
@@ -50,3 +50,85 @@ cout<< beds_sum <<endl;
 // max_sum = sum ;
 
 }
+
+// algorithm
+double min_ending_here = DBL_MAX;
+double min_so_far_d = DBL_MAX;
+double min_so_far = DBL_MAX;
+double length;
+
+
+int max_length;
+double sum=0;
+}
+*/
+
+/*
+for (int i=0; i<M; i++)
+{
+      // if min_ending_here > 0, then it could not possibly
+      // contribute to the minimum sum further
+      if (min_ending_here > 0)
+      {    min_ending_here = bedsperday[i];
+          length=0;
+      }
+      // else add the value arr[i] to min_ending_here
+      else
+      {  min_ending_here += bedsperday[i];
+         length++ ;
+      }
+      // update min_so_far
+
+      min_so_far_d = max( (min_so_far_d/length) , min_ending_here);
+cout<<min_so_far_d<<endl;
+      min_so_far = min(min_so_far , min_ending_here);
+}
+
+//  beds_sum = min_so_far;
+
+
+cout<< length<<endl;
+cout<< min_so_far <<endl;
+cout<<min_so_far_d<<endl;
+// sum = beds_sum / N*arr_sum ;
+*/
+
+//  if (sum > max_sum)
+// max_sum = sum ;
+
+
+
+
+// algorithm
+double min_ending_here = DBL_MAX;
+double min_so_far_d = DBL_MAX;
+double min_so_far = DBL_MAX;
+double length;
+
+
+int max_length;
+double sum=0;
+
+for (int i=0; i<M; i++)
+{
+      if (fun(min_ending_here,N*length) < 1)
+      {   min_ending_here = bedsperday[i];
+          length=0;
+      }
+
+      else
+      {
+         if( fun(min_ending_here,N*length) > sum ) {
+
+           sum = fun(min_ending_here,N*length);
+           max_length=length;
+          }
+
+          length++ ;
+          min_ending_here += bedsperday[i];
+
+      }
+}
+cout<<sum<<endl;
+
+cout<< max_length<<endl;
