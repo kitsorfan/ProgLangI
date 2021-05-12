a9865fe791ed696b46d73206833f65ea320397df
kitsorfan <kitsorfan@protonmail.com>
2021-05-12 16:44:28 +0300

    add series2

---
+++ 2nd Seiries/qssort/qssort.py
@@ -0,0 +1,10 @@
+# TO DO
+#
+from os import O_NDELAY
+
+
+# 🔵Read the input (easy)
+# 🔵Sort the data (easy)
+# 🔵Create nodes (queue, stack, pointerQ, pointerS) (medium)
+# 🔵Add nodes creating graph till find the desirable One (check if node already exists in the graph) (hard x2)
+# 🔵Find minimum path to that node (medium)
