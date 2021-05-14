c6ac02e146450bf2133af34f65d651f0f16e72c7
kitsorfan <kitsorfan@protonmail.com>
2021-05-14 13:21:58 +0300

    add program description

--- loop_rooms/loop_rooms.py
+++ loop_rooms/loop_rooms.py
@@ -1,5 +1,50 @@
-import sys
 
+"""
+********************************************************************************
+  Project     : Programming Languages 1 - Assignment 2 - Exercise LoopRooms
+  Authors     : Stylianos Kandylakis el17088, 
+                Kitsos Orfanopoulos el17025
+  -----------
+ ProgLangI 2021, ECE-NTUA
+ *******************************************************************************
+
+>> TRANSLATING THE C++ PROBLEM:
+---------------------------------
+    At first we tried to "translate" our C++ code (*) to Python. That worked
+    pretty much well for small testcases. Though for larger ones we exceeded
+    the maximum recursion depth. Even with increasing the recursion limit 
+    (sys.setrecursionlimit(5000)) the problem could not be solved, possibly
+    due to memory problems. Python is not as fast as C++ and its performance 
+    for recursion is very poor.
+
+    (*): Our C++ solution was a RECURSIVE DFS at a double array. We follow
+        the path of every door; if we found an exit the path was "good", else
+        if we found an already visited room the path was "bad". 
+
+>> ITERATIVE ADAPTATION:
+----------------------------
+    Our recursive DFS should be written in an iterative way. The Idea is to
+    follow the path (do a DFS) until you find the last room, the door of which 
+    leads to an exit/visited room (that will be the "paint"). Then we follow 
+    again the same path from initial room to the last room, "painting" each 
+    room with the same "paint". 
+    "Paint" is the escapability of each room, 
+        0 is unknown (initial value), 
+        1 is escapable, 
+        2 is visited=non escapable. 
+    So in every step we follow twice each path. 
+    At the end we scan our array to count escapable rooms.
+
+>> STEPS:
+----------------------------
+    1. Read input and create arrays.
+    2. Start following paths (DFS) from every unvisited room.
+    3. Every time you finish one path, follow it again to "paint" it.
+    4. Count escapable rooms and print them.
+"""
+
+
+import sys #for reading input from terminal
 
 
 def check_next(door, escapes,i,j,N,M):
@@ -73,9 +118,9 @@ def check_path(doors, escapes, i, j, N, M):
 
         
 
-# Main
-if __name__ == "__main__":
+# @@@@@@@@@@@@@@@@@@@@@@@@@@@--- MAIN ---@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
+if __name__ == "__main__":
     # Reading Data
     with open(sys.argv[1], "rt") as inputFile: # open the file, name as 
         N, M = map(int, inputFile.readline().split())
