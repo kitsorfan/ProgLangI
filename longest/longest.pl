be0c9e681e77030781990d6e0828608d358bec9c
kitsorfan <kitsorfan@protonmail.com>
2021-05-27 02:41:30 +0300

    partially working

--- longest/longest.pl
+++ longest/longest.pl
@@ -16,10 +16,6 @@ read_line(Stream, L) :-
 
 
 
-
-
-
-
 % *@@@@@@@@@@@@@@@@@@-  Minus Hospitals -@@@@@@@@@@@@@@@@@@*
 % A function that takes a list L1 and the Hospitals number 
 % and returns a list L2 where L2[i]=-L1[i]-Hospitals
@@ -44,115 +40,57 @@ indexAndPrefix(Index,Sum,[X|More1],[[NewSum,Index]|More2]):-
 
 addZero(A,[[0,0]|A]).
 
-
-% % halve from: https://stackoverflow.com/questions/10063838/understanding-the-splitting-in-swi-prolog
-% halve(List,A,B) :- halve(List,List,A,B), !.
-% halve(B,[],[],B).
-% halve(B,[_],[],B).
-% halve([H|T],[_,_|T2],[H|A],B) :-halve(T,T2,A,B). 
-
-% % https://stackoverflow.com/questions/15926034/how-to-merge-lists-in-prolog/15926279
-
-% m2([A|As], [B|Bs], [A,B|Rs]) :-
-%     A=[A1,_],
-%     B=[B1,_],
-%     A1=<B1,
-%     !, m2(As, Bs, Rs).
-% m2([A|As], [B|Bs], [B,A|Rs]) :-
-%     !, m2(As, Bs, Rs).
-% m2([], Bs, Bs) :- !.
-% m2(As, [], As).
-
-% mergesort(Unsorted, Sorted):-
-%     halve(Unsorted,X,Y),
-%     writeln('Inside the matrix'),
-%     writeln(X),
-%     writeln(Y),
-%     mergesort(X,Sorted1),
-%     % mergesort(Y,Sorted2),   
-%     % mergesort(Y,Sorted2),
-%     m2(Sorted1,Y,Sorted),!.
-% mergesort([],[]).
-% % mergesort(A,A).
-% % m2([A|As], [B|Bs], [B,A|Rs]) :-
-% %     nth0(A1,A,0),
-% %     nth0(B1,B,0),
-% %     A1>B1,
-% %     !, m2(As, Bs, Rs).
-
-/* merge_sort(Xs, Ys) is true if Ys is a sorted permutation of the list Xs.*/
-% merge_sort(List, SortedList):-
-%     lengthy(List, Length),
-%     merge_sort_1(Length, List, SortedList, []).
-   
-  /* merge_sort_1(N, Xs, Ys, Zs) is true if Ys is a sorted permutation of    */
-  /*  the first N elements of the list Xs, and Zs are the remaining elements */
-%   /*  of Xs.                                                                 */
-%   merge_sort_1(0, Rest, [], Rest):-!.
-%   merge_sort_1(1, [A|Rest], [A], Rest):-!.
-%   merge_sort_1(2, [A,B|Rest], C, Rest):-
-
-%     A=<B,
-%      !,C = [A,B].
-%   merge_sort_1(2, [A,B|Rest], C, Rest):- !,C = [B,A].
-%   merge_sort_1(N, List, Sorted, Rest):-
-%     N1 = N div 2, N2 = N - N1,
-%     merge_sort_1(N1, List, SortedLeft, TempList),
-%     merge_sort_1(N2, TempList, SortedRight, Rest),
-%     ordered_merge(SortedLeft, SortedRight, Sorted).
-   
-%   /* ordered_merge(Xs, Ys, Zs) is true if Zs is an ordered list obtained     */
-%   /*   from merging the ordered lists Xs and Ys.                             */
-%   ordered_merge([X|Xs], [Y|Ys], [Y|Zs]):-
-%     Y=[Y1,_],
-%     X=[X1,_],
-%     Y1 =< X1, 
-%     ordered_merge([X|Xs], Ys, Zs).
-% ordered_merge([X|Xs], Ys, [X|Zs]):-
-% ordered_merge(Xs, Ys, Zs).
-% ordered_merge([], Ys, Ys).
-
-%   /* length(Xs, L) is true if L is the number of elements in the list Xs.    */
-%   lengthy(Xs, L):-length_1(Xs, 0, L).
-  
-%   /* length_1(Xs, L0, L) is true if L is equal to L0 plus the number of      */
-%   /*   elements in the list Xs.                                              */
-%   length_1([], L, L).
-%   length_1([_|Xs], L0, L):-L1 = L0 + 1, length_1(Xs, L1, L).
-
-% merge([],[],[]).
-% merge([],A,A).
-% merge(B,[],B).
-% merge([A|Arest],[B|Brest],Merged):-
-%     nth0(X,A,0),
-%     nth0(Y,B,0),
-%     X<Y,
-%     merge(Arest,Brest,Merged).
-
-
-% mergesort([],[]).
-% mergesort(A,A).
-% mergesort(Unsorted,Sorted):-
-    
-            
-%         fun merge (nil, a) = a 
-%         |   merge (b , nil) = b
-%         |   merge (x::xs, y::ys) =
-%             if tupleMin(x,y) then x::merge(xs, y::ys) 
-%             else y::merge(ys, x::xs);
-
-            
-%             val (x,y) = halve merge_list
-%        in
-%             merge(mergesort x, mergesort y)
-%         end;
-
+% *@@@@@@@@@@@@@@@@@@- Sort -@@@@@@@@@@@@@@@@@@*
 
 mycompare(<,[A1,_],[A2,_]) :- A1 < A2.
 mycompare(>, _, _).
 
 mySort(Unsorted,Sorted):-
-predsort(mycompare, Unsorted, Sorted),!.
+    predsort(mycompare, Unsorted, Sorted),!.
+
+% *@@@@@@@@@@@@@@@@@@- Remove Prefix -@@@@@@@@@@@@@@@@@@*
+% from tuple to single list
+
+
+removePrefix([],[]).
+removePrefix([[_,Index]|More1],[Index|More2]):-
+    removePrefix(More1,More2).
+
+% *@@@@@@@@@@@@@@@@@@- Find max Dif -@@@@@@@@@@@@@@@@@@*
+
+findMaxDif([],_,_).
+findMaxDif([A|Rest],Min,MaxDif):-
+    write('Min is '),
+    write(Min),
+    write(' and MaxDif is '),
+    writeln(MaxDif),
+    A<Min,
+    NewMin = A,
+    Dif is (A-Min),
+    Dif>MaxDif,
+    NewMaxDif = Dif,
+    findMaxDif(Rest,NewMin,NewMaxDif).
+findMaxDif([A|Rest],Min,MaxDif):-
+    A<Min,
+    NewMin = A,
+    Dif is (A-Min),
+    Dif=<MaxDif,
+    NewMaxDif = MaxDif,
+    findMaxDif(Rest,NewMin,NewMaxDif).
+findMaxDif([A|Rest],Min,MaxDif):-
+    A>=Min,
+    NewMin = Min,
+    Dif is (A-Min),
+    Dif>MaxDif,
+    NewMaxDif = Dif,
+    findMaxDif(Rest,NewMin,NewMaxDif).
+findMaxDif([A|Rest],Min,MaxDif):-
+    A>=Min,
+    NewMin = Min,
+    Dif is (A-Min),
+    Dif=<MaxDif,
+    NewMaxDif = MaxDif,
+    findMaxDif(Rest,NewMin,NewMaxDif).
 
 
 % @@@@@@@@@@@@@@@@@@@@@@@@- MAIN FUNCTION -@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
@@ -162,7 +100,13 @@ longest(File, Answer) :-
     indexAndPrefix(1,0,MinusDischarges,IndexedDischarges),
     addZero(IndexedDischarges,ZeroIndexedDischarges),
     mySort(ZeroIndexedDischarges,SortedDischarges),
-    writeln(SortedDischarges),
+    removePrefix(SortedDischarges,SortedIndexes),
+    writeln(SortedIndexes),
+    SortedIndexes = [First|_],
+    writeln(First),
+    Answer is 0,
+    findMaxDif(SortedIndexes,First,Answer),
+    writeln('Answer is '),
     fail.
 
 
